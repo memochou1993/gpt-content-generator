1b5898323c1f1d4b4a69e63f621b832e0417b61f
Memo Chou <memochou1993@gmail.com>
2023-02-17 12:36:05 +0800

    Add ContentGenerator component

---
+++ src/components/ContentGenerator.vue
@@ -0,0 +1,110 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <v-row class="justify-center column">
+    <v-col cols="12" sm="10" md="6">
+      <v-card
+        color="blue-grey-lighten-5"
+      >
+        <v-card-item class="pa-8">
+          <div class="text-h5 mb-8 font-weight-bold text-indigo">
+            文案產生器
+          </div>
+          <div class="my-4">
+            <div class="title mb-2">
+              題材
+            </div>
+            <div>
+              <v-textarea
+                autofocus
+                color="indigo"
+                hide-details
+                no-resize
+                rows="3"
+                variant="outlined"
+              />
+            </div>
+          </div>
+          <div class="my-4">
+            <div class="title mb-2">
+              語氣
+            </div>
+            <div>
+              <v-btn color="indigo" active variant="outlined" class="mr-2 mb-2">
+                專業
+              </v-btn>
+              <v-btn color="indigo" variant="outlined" class="mr-2 mb-2">
+                有趣
+              </v-btn>
+              <v-btn color="indigo" variant="outlined" class="mr-2 mb-2">
+                嚴肅
+              </v-btn>
+            </div>
+          </div>
+          <div class="my-4">
+            <div class="title mb-2">
+              格式
+            </div>
+            <div>
+              <v-btn color="indigo" active variant="outlined" class="mr-2 mb-2">
+                段落
+              </v-btn>
+              <v-btn color="indigo" variant="outlined" class="mr-2 mb-2">
+                郵件
+              </v-btn>
+              <v-btn color="indigo" variant="outlined" class="mr-2 mb-2">
+                文章
+              </v-btn>
+              <v-btn color="indigo" variant="outlined" class="mr-2 mb-2">
+                詩詞
+              </v-btn>
+            </div>
+          </div>
+          <div class="my-4">
+            <div class="title mb-2">
+              長度
+            </div>
+            <div>
+              <v-btn color="indigo" active variant="outlined" class="mr-2 mb-2">
+                短
+              </v-btn>
+              <v-btn color="indigo" variant="outlined" class="mr-2 mb-2">
+                中
+              </v-btn>
+              <v-btn color="indigo" variant="outlined" class="mr-2 mb-2">
+                長
+              </v-btn>
+            </div>
+          </div>
+        </v-card-item>
+        <v-card-actions class="justify-end pa-8 pt-0">
+          <v-btn color="indigo" variant="outlined" block>
+            產生草稿
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="10" md="6">
+      <v-card
+        color="blue-grey-lighten-5"
+      >
+        <v-card-item class="pa-8">
+          <div class="text-h5 mb-8 font-weight-bold text-indigo">
+            生成結果
+          </div>
+        </v-card-item>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
